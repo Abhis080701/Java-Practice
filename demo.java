abc09df5d24b81237e9c49e77993b6ccf6af7058
Abhis080701 <abhis080701@gmail.com>
2025-01-23 13:28:46 +0530

    delete the class file

--- demo.java
+++ demo.java
@@ -1,8 +1,10 @@
-class A{
-    final public int calculate(int a,int b){return 1;}
+class A {
+    final public int calculate(int a, int b) {
+        return 1;
+    }
 }
 
 public class demo extends A {
-    
-//public int calculate(int a,int b){return 2;}
+
+    // public int calculate(int a,int b){return 2;}
 }
